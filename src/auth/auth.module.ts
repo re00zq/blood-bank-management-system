520f2f428edd12cb1fee1407d472956a632f90ee
Mahmoud Rezq <mahmoud.m.rezq@gmail.com>
2025-07-17 02:04:07 +0300

    fix(donor): fix dependency injection and service usage

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { LoginService } from './services/login-donor.service';
 import { AuthController } from './auth.controller';
 import { GetAccessTokenService } from './services/get-access-token.service';
 import { RegisterService } from './services/register-donor.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { DonorModule } from 'src/donor/donor.module';
 
 @Module({
   controllers: [AuthController],
   providers: [GetAccessTokenService, LoginService, RegisterService],
+  imports: [
+    JwtModule.register({}),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    DonorModule,
+  ],
 })
 export class AuthModule {}
--- src/donor/donor.module.ts
+++ src/donor/donor.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { DonorController } from './donor.controller';
 import { Donor } from './entities/donor.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateDonorService } from './services/create-donor.service';
+import { ListDonorService } from './services/list-donors.service';
+import { FindDonorService } from './services/find-donor.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Donor])],
   controllers: [DonorController],
-  providers: [],
+  providers: [CreateDonorService, ListDonorService, FindDonorService],
+  exports: [FindDonorService, CreateDonorService],
 })
 export class DonorModule {}
